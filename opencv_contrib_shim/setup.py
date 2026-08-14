a5de8353e42fba8c3ea7c2f91f5cb507d035bc8b
ryusen-lhj <limhj-wm23@student.tarc.edu.my>
2026-08-14 15:43:55 +0800

    Add OpenCV metadata shim for Streamlit Cloud

---
+++ opencv_contrib_shim/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="opencv-contrib-python",
+    version="4.11.0.86",
+    description="Metadata-only shim so MediaPipe can use headless OpenCV on Streamlit Cloud.",
+    py_modules=[],
+)
